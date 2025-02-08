af5f047d3868ef19d21c93de7b51534f5ec9f7cb
Ericson Willians Rocha Pires <ericsonwillians@protonmail.com>
2025-02-07 21:00:08 -0300

    Updates

--- src/components/layout/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export function Footer() {
-    return (
-      <footer className="mt-12 border-t border-border pt-4">
-        <p className="text-xs text-green-400/75">
-          &copy; {new Date().getFullYear()} Ericson Willians. All rights reserved.
-        </p>
-      </footer>
-    );
-  }
--- src/utils/midi.ts
+++
@@ -1,3 +0,0 @@
-export const getMIDIFrequency = (midiNumber: number): number => {
-    return 440 * Math.pow(2, (midiNumber - 69) / 12);
-  };
